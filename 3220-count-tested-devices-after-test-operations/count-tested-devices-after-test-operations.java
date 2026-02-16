91cef6c5160692507f428cc0cac76f9abd6f17c6
irfanahsan393-lab <irfanahsan393@gmail.com>
2026-02-16 22:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (86.89%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int count = 0;
+        
+        for (int i = 0; i < batteryPercentages.length; i++) {
+            if (batteryPercentages[i] - count > 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
